eb3aa66485e7015e6c56ec422dcb2a1d67f52241
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-07-09 22:06:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Button from './components/Button/Button.vue'
+
+</script>
+
+<template>
+  <main>
+    <Button ref="buttonRef">Test Button</Button>
+  </main>
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  border: 1px solid green;
+}
+.vk-tooltip__popper {
+  border: 1px solid red;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
